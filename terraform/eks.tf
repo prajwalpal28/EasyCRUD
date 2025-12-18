3abd6bb23b474f2eaf585eea162b44257d9a38f3
prajwalpal28 <palprajwal18@gmail.com>
2025-12-18 17:23:38 +0530

    Refactor EKS module configuration for improved readability

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,22 +1,19 @@
 module "eks" {
-source = "terraform-aws-modules/eks/aws"
-version = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.29"
 
-cluster_name = "${var.project_name}-cluster"
-cluster_version = "1.29"
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = aws_subnet.private[*].id
 
-
-vpc_id = aws_vpc.this.id
-subnets = aws_subnet.private[*].id
-
-
-eks_managed_node_groups = {
-default = {
-instance_types = ["t3.medium"]
-desired_size = 2
-min_size = 1
-max_size = 3
-}
-}
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+    }
+  }
 }
