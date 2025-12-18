c07ccf0f7cf54828a7bd63d6cae7bc17acbaa4a9
prajwalpal28 <palprajwal18@gmail.com>
2025-12-18 17:28:55 +0530

    Refactor VPC and subnet resources for improved structure and readability

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,61 +1,36 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "this" {
-cidr_block = var.vpc_cidr
-enable_dns_support = true
-enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-
-tags = {
-Name = "${var.project_name}-vpc"
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
 }
-}
-
-
-resource "aws_internet_gateway" "igw" {
-vpc_id = aws_vpc.this.id
-}
-
 
 resource "aws_subnet" "public" {
-count = 2
-vpc_id = aws_vpc.this.id
-cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
-map_public_ip_on_launch = true
-availability_zone = data.aws_availability_zones.available.names[count.index]
-}
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
+  tags = {
+    Name = "${var.project_name}-public-${count.index}"
+  }
+}
 
 resource "aws_subnet" "private" {
-count = 2
-vpc_id = aws_vpc.this.id
-cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
-availability_zone = data.aws_availability_zones.available.names[count.index]
-}
+  count             = 2
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-
-resource "aws_route_table" "public_rt" {
-vpc_id = aws_vpc.this.id
-
-
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.igw.id
-}
-}
-
-
-resource "aws_route_table_association" "public_assoc" {
-count = 2
-subnet_id = aws_subnet.public[count.index].id
-route_table_id = aws_route_table.public_rt.id
-}
-
-
-resource "aws_eip" "nat" {
-domain = "vpc"
-}
-
-
-resource "aws_nat_gateway" "nat" {
-allocation_id = aws_eip.nat.id
-subnet_id = aws_subnet.public[0].id
+  tags = {
+    Name = "${var.project_name}-private-${count.index}"
+  }
 }
