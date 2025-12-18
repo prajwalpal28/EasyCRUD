1c19f40a43687da89559a011b0847149cc632d72
prajwalpal28 <palprajwal18@gmail.com>
2025-12-18 22:44:52 +0530

    Update AWS provider version in Terraform configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,15 @@
 terraform {
-required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"
 
-
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.23.0"
+    }
+  }
 }
-}
-}
-
 
 provider "aws" {
-region = var.region
-profile = "configs"
+  region  = var.region
+  profile = "configs"
 }
